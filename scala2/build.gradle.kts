85b611755942c226ca1c7fd64fce4169ec2dc21a
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-25 23:58:12 +0300

    upgrade to Selenide 7.0.0 and Java 17

--- scala2/build.gradle.kts
+++ scala2/build.gradle.kts
@@ -11,10 +11,10 @@ dependencies {
     implementation("org.scala-lang:scala-library:2.13.12")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-inline:4.11.0")
-    testImplementation("org.scalatest:scalatest_2.13:3.3.0-SNAP4")
-    testImplementation("org.scalatestplus:junit-4-12_2.13:3.3.0.0-SNAP2")
-    testImplementation("com.codeborne:selenide:6.19.1")
+    testImplementation("org.mockito:mockito-core:5.6.0")
+    testImplementation("org.scalatest:scalatest_2.13:3.2.17")
+    testImplementation("org.scalatestplus:junit-4-12_2.13:3.2.2.0")
+    testImplementation("com.codeborne:selenide:7.0.0")
 
     testRuntimeOnly("org.scala-lang.modules:scala-xml_2.13:2.2.0")
 }
