e8a62db68c1a48a515f1c7f6180532811e69c64b
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-10-01 11:40:45 +0300

    enable Selenide logs in tests

--- scala2/build.gradle.kts
+++ scala2/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     testImplementation("com.codeborne:selenide:7.10.1")
 
     testRuntimeOnly("org.scala-lang.modules:scala-xml_2.13:2.4.0")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.17")
 }
 
 tasks {
