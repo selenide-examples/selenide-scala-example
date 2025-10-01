49fa6b9a41cad2eb9f5f452b1561e407714d1f53
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-10-01 11:55:41 +0300

    update web test to work in headless mode

--- scala2/src/test/scala/org/selenide/examples/scala2/SelenideTest.java
+++ scala2/src/test/scala/org/selenide/examples/scala2/SelenideTest.java
@@ -2,13 +2,24 @@ package org.selenide.examples.scala2;
 
 import org.junit.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideTest {
   @Test
   public void webTestSample() {
-    open("https://duckduckgo.com");
-    $("[name=q]").val("Selenide Scala").pressEnter();
+    open("https://selenide.org");
+    $(".main-menu-pages").find(byText("Users")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(size(1));
+
+    $$("#user-tags .tag").shouldHave(sizeGreaterThan(8));
+
+    $("#user-tags").find(byTagAndText("a", "all")).click();
+    $$("#selenide-users .user:not(.hidden)").shouldHave(sizeGreaterThan(80));
   }
 }
